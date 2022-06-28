8960144009842d2555601feb9c1779b75ae3f60e
Justin Chun-ting Ho <25782338+justinchuntingho@users.noreply.github.com>
2022-06-28 17:00:22 +0800

    init

---
+++ 1_textanalysis.R
@@ -0,0 +1,215 @@
+library(tidyverse)
+library(quanteda)
+library(quanteda.textplots)
+library(quanteda.textstats)
+
+
+
+####################################################################################
+## Basic Text Analysis                                                            ##
+####################################################################################
+
+# Loading the documents
+df_ukraine <-  read.csv("ukraine.csv")
+corpus_ukraine <- corpus(df_ukraine, text_field = "text")
+
+# The followings are not necessary steps, but it is always a good idea to view a portion of your data
+corpus_ukraine[1:10] # print the first 10 documents
+ndoc(corpus_ukraine) # Number of Documents
+nchar(corpus_ukraine[1:10]) # Number of character for the first 10 documents
+ntoken(corpus_ukraine[1:10]) # Number of tokens for the first 10 documents
+ntoken(corpus_ukraine[1:10], remove_punct = TRUE) # Number of tokens for the first 10 documents after removing punctuation
+
+# Meta-data
+head(docvars(corpus_ukraine))
+
+# Creating DFM
+tokens_ukraine <- tokens(corpus_ukraine, 
+                     remove_punct = TRUE, 
+                     remove_numbers = TRUE, 
+                     remove_url = TRUE,
+                     verbose = TRUE)
+dfm_ukraine <- dfm(tokens_ukraine)
+
+# Inspecting the results
+topfeatures(dfm_ukraine, 30)
+
+# What do they save about russian?
+kwic(tokens_ukraine, "russian", 3)
+
+# Plotting a histogram
+features <- topfeatures(dfm_ukraine, 100)  # Putting the top 100 words into a new object
+data.frame(list(term = names(features), frequency = unname(features))) %>% # Create a data.frame for ggplot
+  ggplot(aes(x = reorder(term,-frequency), y = frequency)) + # Plotting with ggplot2
+  geom_point() +
+  theme_bw() +
+  labs(x = "Term", y = "Frequency") +
+  theme(axis.text.x=element_text(angle=90, hjust=1))
+
+# Doing it again, removing stop words this time!
+
+# Defining custom stopwords
+customstopwords <- c("amp", "just", "make", "stopword")
+
+dfm_ukraine <- dfm_remove(dfm_ukraine, c(stopwords('english'), customstopwords))
+
+# Inspecting the results again
+topfeatures(dfm_ukraine, 30) 
+
+# Top words again
+features <- topfeatures(dfm_ukraine, 100)  # Putting the top 100 words into a new object
+data.frame(list(term = names(features), frequency = unname(features))) %>% # Create a data.frame for ggplot
+  ggplot(aes(x = reorder(term,-frequency), y = frequency)) + # Plotting with ggplot2
+  geom_point() +
+  theme_bw() +
+  labs(x = "Term", y = "Frequency") +
+  theme(axis.text.x=element_text(angle=90, hjust=1))
+
+# Wordcloud
+textplot_wordcloud(dfm_ukraine)
+
+####################################################################################
+## Keyword Analysis                                                               ##
+####################################################################################
+
+# Before and After the Invasion
+tstat_key <- textstat_keyness(dfm_ukraine, 
+                              target = docvars(dfm_ukraine, "war"))
+
+head(tstat_key)
+tail(tstat_key)
+
+textplot_keyness(tstat_key)
+
+
+####################################################################################
+## Chinese Text Analysis                                                          ##
+####################################################################################
+
+# Doing text analysis can be tricky, but doing Chinese text analysis can be even trickier
+# There are a few extra things we need to consider 
+
+######################### Showing Chinese Figures ######################### 
+df_ios <-  read.csv("ios.csv")
+corpus_ios <- corpus(df_ios, text_field = "message")
+
+# Creating DFM
+tokens_ios <- tokens(corpus_ios, 
+                     remove_punct = TRUE, 
+                     remove_numbers = TRUE, 
+                     remove_url = TRUE,
+                     remove_symbols = TRUE,
+                     verbose = TRUE)
+dfm_ios <- dfm(tokens_ios)
+
+customstopwords <- c("與", "年", "月", "日")
+
+dfm_ios <- dfm_remove(dfm_ios, c(stopwords('chinese', source = "misc"), stopwords('english'), customstopwords))
+
+# Inspecting the results again
+topfeatures(dfm_ios, 30) 
+
+textplot_wordcloud(dfm_ios)
+
+# You would notice Chinese characters are not displayed correctly in the previous graph.
+# This is because the default font in R doesn't support Chinese
+# To fix it, we simply need to change the font
+# Changing font in R can be tricky, but luckily these packages make it easier
+library(sysfonts)
+font_add_google("Noto Sans TC", "Noto Sans TC")
+library(showtext)
+showtext_auto()
+
+textplot_wordcloud(dfm_ios)
+
+
+######################### Tokenisation ######################### 
+
+# Tokenisation is the process of splitting a string into tokens (in most cases, one token is one word)
+# In Latin languages, tokenisation is much easier since splitting the sentence by the space will do the trick
+# Since there is no space
+
+text <- "one two 福利 one two 福利 happy everyday one two 福利 one two 福利
+福利熊 熊福利 熊福利 福利熊"
+
+# Using the default quanteda tokens() function
+print(tokens(text), max_ntoken = 100)
+
+# It works, but not perfectly
+# There is another way to do it: Jieba
+# Jieba is one of the most widely used packages for tokenisation
+# One advantage of using Jieba is that you can add your own words into the dictionary
+
+# install.packages("jiebaR")
+library(jiebaR)
+
+# Vanila version
+tokeniser <- worker()
+segment(text, tokeniser)
+
+# Adding user defined words
+new_user_word(tokeniser, c("福利熊","熊福利"))
+segment(text, tokeniser)
+
+edit_dict()
+
+# Using jieba with quanteda needs a bit of hacking
+raw_texts <- as.character(corpus_ios) # getting text from the corpus
+tokenised_texts <- purrr::map(raw_texts, segment, tokeniser) # applying jieba::segment() function across rows
+tokens_ios <- tokens(tokenised_texts, # Changing it back to a tokens object
+                     remove_punct = TRUE, 
+                     remove_numbers = TRUE, 
+                     remove_url = TRUE,
+                     remove_symbols = TRUE,
+                     verbose = TRUE)
+tokens_ios
+
+new_user_word(tokeniser, c("今日","台灣"))
+
+dfm_ios <- dfm(tokens_ios)
+dfm_ios <- dfm_remove(dfm_ios, c(stopwords('chinese', source = "misc"), stopwords('english'), customstopwords))
+
+topfeatures(dfm_ios, 30) 
+textplot_wordcloud(dfm_ios)
+
+
+####################################################################################
+## Sentiment Analysis                                                             ##
+####################################################################################
+
+# We will use the Lexicoder Sentiment Dictionary (2015)
+data_dictionary_LSD2015
+
+# Passing the dictionary to the dictionary function, you can also define your own dictionary
+sentiment <- tokens_lookup(tokens_ukraine, data_dictionary_LSD2015) %>% 
+  dfm()
+sentiment <- convert(sentiment, to = "data.frame")
+sentiment["sentiment"] <- sentiment$positive + sentiment$neg_negative - sentiment$negative - sentiment$neg_positive
+
+# Merging the result with the original data set
+df_wsentiment <- cbind(df_ukraine, sentiment)
+
+# Basic Exploration: mean sentiment score
+df_wsentiment %>% 
+  group_by(war) %>% 
+  summarise(sentiment = mean(sentiment))
+
+# Sentiment trend over time
+df_wsentiment$date <- as.Date(df_wsentiment$created_at)
+
+plot_df <- df_wsentiment %>% 
+  group_by(date) %>% 
+  summarise(sentiment = mean(sentiment))
+ggplot(plot_df, aes(date, sentiment)) +
+  geom_line()
+
+# Here we are going to use a function from this package to aggregate by week/month
+# install.packages("lubridate")
+library(lubridate)
+
+plot_df <- df_wsentiment %>% 
+  mutate(date = floor_date(date, "week")) %>% 
+  group_by(date) %>% 
+  summarise(sentiment = mean(sentiment))
+ggplot(plot_df, aes(date, sentiment)) +
+  geom_line()
---
+++ 2_topicmodel.R
@@ -0,0 +1,333 @@
+library(topicmodels)
+library(quanteda)
+library(tidyverse)
+library(lubridate)
+library(sysfonts)
+font_add_google("Noto Sans TC", "Noto Sans TC")
+library(showtext)
+showtext_auto()
+library(jiebaR)
+
+
+####################################################################################
+## Creating DFMs                                                                  ##
+####################################################################################
+
+df_tsai <-  read.csv("tsai.csv")
+corpus_tsai <- corpus(df_tsai, text_field = "Message")
+
+tokeniser <- worker()
+raw_texts <- as.character(corpus_tsai)
+tokenised_texts <- purrr::map(raw_texts, segment, tokeniser)
+tokens_tsai <- tokens(tokenised_texts,
+                     remove_punct = TRUE, 
+                     remove_numbers = TRUE, 
+                     remove_url = TRUE,
+                     remove_symbols = TRUE,
+                     verbose = TRUE)
+dfm_tsai <- dfm(tokens_tsai)
+customstopwords <- c()
+dfm_tsai <- dfm_remove(dfm_tsai, c(stopwords('chinese', source = "misc"), stopwords('english'), customstopwords))
+
+topfeatures(dfm_tsai, 30) 
+
+# Trimming DFM to reduce training time
+docvars(dfm_tsai, "docname") <- docnames(dfm_tsai)
+dfm_trimmed <- dfm_trim(dfm_tsai, min_docfreq = 5, min_count = 10)
+dfm_trimmed
+
+# Removing rows that contain all zeros
+row_sum <- apply(dfm_trimmed , 1, sum)
+dfm_trimmed <- dfm_trimmed[row_sum> 0, ]
+
+# Converting to another format
+lda_data <- convert(dfm_trimmed, to = "topicmodels")
+lda_data
+
+####################################################################################
+## Finding K                                                                      ##
+####################################################################################
+
+
+######################### LDA Tuning ######################### 
+# install.packages("ldatuning")
+library("ldatuning")
+
+ldatuning.result <- FindTopicsNumber(
+  lda_data,
+  topics = seq(from = 10, to = 50, by = 10),
+  metrics = c("Griffiths2004", "CaoJuan2009", "Arun2010"), # There are 4 possible metrics: Griffiths2004", "CaoJuan2009", "Arun2010", "Deveaud2014"
+  method = "Gibbs",
+  control = list(seed = 4321),
+  verbose = TRUE
+)
+FindTopicsNumber_plot(ldatuning.result)
+
+
+######################### Perplexity ######################### 
+
+# Another approach to find K is perplexity: we split the data into 5 parts, 
+# train a model using 4 of the 5 parts, then see how well does it predict the held-out one, repeat 5 times.
+# Note that this approach can take a long time since we need to train 5 models for each candidate K
+
+library(doParallel)
+library(dplyr)
+library(reshape2)
+library(tidyr)
+library(ggplot2)
+
+# Here we do parallelisation to speed up the process.
+cluster <- makeCluster(detectCores(logical = TRUE)-1, outfile = "Log.txt")
+registerDoParallel(cluster)
+clusterEvalQ(cluster, {
+  library(topicmodels)
+})
+
+n <- nrow(lda_data)
+burnin <- 1000
+iter <- 1000
+keep <- 50
+folds <- 5
+splitfolds <- sample(1:folds, n, replace = TRUE)
+candidate_k <- c(10, 20, 30, 40, 50) # candidates for how many topics
+
+clusterExport(cluster, c("lda_data", "burnin", "iter", "keep", "splitfolds", "folds", "candidate_k"))
+
+system.time({
+  results <- foreach(j = 1:length(candidate_k), .combine = rbind) %dopar%{
+    k <- candidate_k[j]
+    results_1k <- matrix(0, nrow = folds, ncol = 2)
+    colnames(results_1k) <- c("k", "perplexity")
+    for(i in 1:folds){
+      train_set <- lda_data[splitfolds != i , ]
+      valid_set <- lda_data[splitfolds == i, ]
+      
+      fitted <- LDA(train_set, k = k, method = "Gibbs",
+                    control = list(burnin = burnin, iter = iter, keep = keep) )
+      results_1k[i,] <- c(k, perplexity(fitted, newdata = valid_set))
+    }
+    print(k)
+    return(results_1k)
+  }
+})
+stopCluster(cluster)
+
+# Plotting the results
+results_df <- as.data.frame(results)
+results_df$istest <- "test"
+avg_perplexity <- results_df %>% group_by(k) %>% summarise(perplexity = mean(perplexity))
+avg_perplexity$istest <- "avg"
+plot_df <- rbind(results_df, avg_perplexity)
+
+ggplot(plot_df, aes(x = k, y = perplexity, group = istest)) +
+  geom_point(aes(colour = factor(istest))) +
+  geom_line(data = subset(plot_df, istest %in% "avg"), color = "red") +
+  ggtitle("5-fold Cross-validation of Topic Modelling") +
+  labs(x = "Candidate k", y = "Perplexity") +
+  scale_x_discrete(limits = candidate_k) +
+  scale_color_discrete(name="Test\\Average",
+                       breaks=c("test", "avg"),
+                       labels=c("Test", "Average")) +
+  theme_bw() +
+  theme(plot.title = element_text(hjust = 0.5))
+
+
+####################################################################################
+## Fit Optimal Model(s)                                                           ##
+####################################################################################
+
+# A good practice would be to fit multiple models from our K search and compare their performance
+# But here we are fitting a toy model to save time
+
+lda_model <- LDA(lda_data, 10, method="Gibbs")  
+
+get_terms(lda_model, k=20)
+
+
+####################################################################################
+##  Visualize Terms                                                               ##
+####################################################################################
+library(tidytext)
+library(tidyr)
+
+# First we need to extract the beta (probability of a word in a topic)
+topics <- tidy(lda_model, matrix = "beta")
+
+topics %>%
+  group_by(topic) %>%
+  top_n(10, beta) %>%
+  ungroup() %>%
+  arrange(topic, -beta) %>% 
+  mutate(term = reorder_within(term, beta, topic)) %>%
+  ggplot(aes(beta, term, fill = factor(topic))) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~ topic, scales = "free") +
+  scale_y_reordered()
+
+######################### LDAvis ##############################
+library(topicmodels)
+library(dplyr)
+library(stringi)
+library(quanteda)
+library(LDAvis)
+
+# LDAvis is an interactive tool to visualise a LDA model
+# It is useful for initial exploration, especially the relationship between topics
+
+# There might be a discrepancy between the dfm we feed the function,
+# and the dfm actually used for training (some rows might be excluded)
+visdfm <- dfm_subset(dfm_trimmed, docname %in% rownames(lda_data))
+
+# A custom function to transform data to json format
+topicmodels_json_ldavis <- function(fitted, dfm, dtm){
+  # Find required quantities
+  phi <- posterior(fitted)$terms %>% as.matrix
+  theta <- posterior(fitted)$topics %>% as.matrix
+  vocab <- colnames(phi)
+  doc_length <- ntoken(dfm)
+  
+  temp_frequency <- as.matrix(dtm)
+  freq_matrix <- data.frame(ST = colnames(temp_frequency),
+                            Freq = colSums(temp_frequency))
+  rm(temp_frequency)
+  # Convert to json
+  json_lda <- LDAvis::createJSON(phi = phi, theta = theta,
+                                 vocab = vocab,
+                                 doc.length = doc_length,
+                                 term.frequency = freq_matrix$Freq)
+  return(json_lda)
+}
+
+json_lda <- topicmodels_json_ldavis(lda_model, visdfm, lda_data)
+serVis(json_lda, open.browser = TRUE)
+
+
+####################################################################################
+##  Validation                                                                    ##
+####################################################################################
+
+# Before we do anything, it is a good idea to have a data frame that contains
+# all document-level information and the topic for each document
+topic_df <- docvars(corpus_tsai)
+topic_df$doc_name <- docnames(corpus_tsai)
+topic_df$text <- as.character(corpus_tsai)
+
+lda_df <- data.frame(topic = get_topics(lda_model), 
+                     doc_name = lda_model@documents)
+topic_df <- left_join(topic_df, lda_df, by = "doc_name")
+topic_df$date <- as.Date(topic_df$Post.Created.Date)
+topic_df$topic <- as.factor(topic_df$topic)
+
+
+######################### Accuracy per Topic ##############################
+
+# Accuracy, this is the bare minimum that we should do
+validation_df <- topic_df %>% 
+  group_by(topic) %>% 
+  slice_sample(n = 10, replace = TRUE)
+
+write.csv(validation_df, "topic_validation.csv")
+
+######################### Word Intrusion Test ##############################
+# install.packages("oolong")
+library(oolong)
+
+oolong_test <- wi(lda_model)
+oolong_test$do_word_intrusion_test()
+oolong_test$lock(force = TRUE)
+oolong_test
+
+######################### Topic Intrusion Test ##############################
+oolong_test <- ti(lda_model, corpus_tsai)
+oolong_test$do_topic_intrusion_test()
+oolong_test$lock(force = TRUE)
+oolong_test
+
+####################################################################################
+##  Visualize Topics                                                              ##
+####################################################################################
+
+# We can see counts
+table(topic_df$topic)
+
+topic_df %>% 
+  group_by(topic) %>% 
+  count() %>% 
+  ggplot(aes(topic, n, fill = topic)) +
+  geom_col() +
+  coord_flip()
+
+
+# Or even trend over time
+topic_df %>% 
+  mutate(date = floor_date(date, "week")) %>% 
+  group_by(date, topic) %>% 
+  count() %>% 
+  ggplot(aes(date, n, color = topic)) +
+  geom_line() +
+  facet_wrap(vars(topic))
+
+####################################################################################
+## Topic Network                                                                  ##
+####################################################################################
+
+# In some cases, it might be useful to visualize a topic model as a network
+# A use case can be found: https://doi.org/10.1111/nana.12805
+
+library(igraph)
+library(ggnewscale)
+library(ggnetwork)
+library(RColorBrewer)
+
+beta_matrix <- lda_model@beta
+beta_matrix <- t(beta_matrix) # Transform it so that col: topic, row: word
+
+# Correlation between columns (word usage)
+cor_matrix <- cor(beta_matrix, method = "pearson")
+colnames(cor_matrix) <- 1:10
+rownames(cor_matrix) <- 1:10
+diag(cor_matrix) <- 0
+quantiles <- quantile(as.vector(cor_matrix), c(.8, .9, .95, .99))
+
+# Turn it into a network
+cor_network <- graph_from_adjacency_matrix(cor_matrix, mode = "upper", weighted = TRUE)
+cor_network <- igraph::delete.edges(cor_network, which(E(cor_network)$weight <= quantiles[1])) # removing all edges below 80%
+
+V(cor_network)$degree <- igraph::degree(cor_network) # Here we use degree centrality, but we can use other measures instead
+
+# Splitting edges into 4 sub-datasets for visualisation
+top99 <- function(x) { x[ x$weight >= quantiles[4], ] }
+top95 <- function(x) { x[ x$weight >= quantiles[3] & x$weight < quantiles[4], ] }
+top90 <- function(x) { x[ x$weight >= quantiles[2] & x$weight < quantiles[3], ] }
+top80 <- function(x) { x[ x$weight < quantiles[2], ] }
+
+ggplot(ggnetwork(cor_network), aes(x = x, y = y, xend = xend, yend = yend)) +
+  geom_edges(aes(size = 0.5),  color = brewer.pal(n = 5, name = "Greys")[2], 
+             curvature = 0.15, alpha = 0.4, show.legend = FALSE,
+             data = top80) +
+  geom_edges(aes(size = 0.75),  color = brewer.pal(n = 5, name = "Greys")[3], 
+             curvature = 0.15, alpha = 0.4, show.legend = FALSE,
+             data = top90) +
+  geom_edges(aes(size = 1), color = "brown4",
+             curvature = 0.15, alpha = 0.4, show.legend = FALSE,
+             data = top95) +
+  geom_edges(aes(size = 1), color = "brown2",
+             curvature = 0.15, alpha = 0.7, show.legend = FALSE,
+             data = top99) +
+  new_scale_color() +
+  geom_nodes(aes(x, y, size = (degree + 1)), alpha = 0.9)+
+  scale_size_area("degree", max_size = 20) +
+  geom_nodelabel_repel(aes(label = name), size = 6, alpha = 0.8, segment.size = 5) +
+  theme_minimal() +
+  theme(axis.text = element_blank(),
+        axis.title = element_blank(),
+        panel.background = element_blank(),
+        panel.grid = element_blank(),
+        legend.text=element_text(size=16),
+        legend.title=element_text(size=16),
+        legend.position = "bottom",
+        legend.background = element_rect(fill="white", size=0.5, linetype="solid")) +
+  guides(size=FALSE, alpha=FALSE, fill=FALSE)
+
+
+
